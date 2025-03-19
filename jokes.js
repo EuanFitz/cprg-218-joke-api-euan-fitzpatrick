92095eef154f20001b87880c5355f990a4aafe27
EuanFitz <euan.fitzpatty@gmail.com>
2025-03-18 18:47:13 -0600

    Adding styles
    Added a lot of functionality with javascript. Everybutton when pressed will replace the text content with a new joke and change the background color according.
    I also changed a lot of styling and added a button in the bottom to give a tip on how to get new jokes.

---
+++ jokes.js
@@ -0,0 +1,88 @@
+jokedisplay.textContent = "Select a joke!";
+
+fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
+.then(response => response.json())
+.then(data => {
+    joketype.textContent = 'Random joke of the day';
+    if(data.joke){
+        jokedisplay.textContent = data.joke;
+    }else{
+        jokedisplay.textContent = data.setup + "... " + data.delivery;
+    }
+});
+fetch("https://v2.jokeapi.dev/joke/programming?safe-mode")
+.then(response => response.json())
+.then(data => {
+    programming.addEventListener('click', function(){
+        joketype.textContent = 'A Random Programming Joke';
+        jokedisplay.style.backgroundColor = 'purple';
+    jokedisplay.style.color = 'white';
+        if(data.joke){
+            jokedisplay.textContent = data.joke;
+        }else{
+            jokedisplay.textContent = data.setup + "... " + data.delivery;
+        }})
+    });
+
+fetch("https://v2.jokeapi.dev/joke/pun?safe-mode")
+.then(response => response.json())
+.then(data => {
+    pun.addEventListener('click', function(){
+        joketype.textContent = 'A Random Pun'
+        jokedisplay.style.backgroundColor = 'red';
+    jokedisplay.style.color = 'black';
+        if(data.joke){
+            jokedisplay.textContent = data.joke;
+        }else{
+            jokedisplay.textContent = data.setup + "... " + data.delivery;
+        }})
+    });
+
+fetch("https://v2.jokeapi.dev/joke/misc?safe-mode")
+.then(response => response.json())
+.then(data => {
+    misc.addEventListener('click', function(){
+        joketype.textContent = 'A Random Miscellaneous Joke';
+        jokedisplay.style.backgroundColor = 'white';
+    jokedisplay.style.color = 'black';
+        if(data.joke){
+            jokedisplay.textContent = data.joke;
+        }else{
+            jokedisplay.textContent = data.setup + "... " + data.delivery;
+        }})
+    });
+
+fetch("https://v2.jokeapi.dev/joke/spooky?safe-mode")
+.then(response => response.json())
+.then(data => {
+   spooky.addEventListener('click', function(){
+    jokedisplay.style.backgroundColor = 'black';
+    jokedisplay.style.color = 'white';
+    joketype.textContent = 'A Random Spooky joke';
+        if(data.joke){
+            jokedisplay.textContent = data.joke;
+        }else{
+            jokedisplay.textContent = data.setup + "... " + data.delivery;
+        }})
+    });
+
+fetch("https://v2.jokeapi.dev/joke/Christmas?safe-mode")
+.then(response => response.json())
+.then(data => {
+    christmas.addEventListener('click', function(){
+        joketype.textContent = 'A Random Christmas Joke';
+        jokedisplay.style.backgroundColor = 'green';
+    jokedisplay.style.color = 'white';
+        if(data.joke){
+            jokedisplay.textContent = data.joke;
+        }else{
+            jokedisplay.textContent = data.setup + "... " + data.delivery;
+        }})
+    });
+
+    openmodal.addEventListener('click', function(){
+        modal.showModal();
+    });
+    closemodal.addEventListener('click', function(){
+        modal.close();
+    });
